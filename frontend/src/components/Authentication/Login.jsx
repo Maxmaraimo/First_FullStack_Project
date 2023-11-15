3bd0c7a5ee9aca65c6e63ca229f2c76a4bf0f8a3
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2023-11-15 19:23:55 +0500

    token verify

---
+++ frontend/src/components/Authentication/Login.jsx
@@ -0,0 +1,58 @@
+import Eye from "../../assets/icons/Eye.png";
+import { useState } from "react";
+import { axiosCall,  accessTokenIsValid, refreshTokenLS} from "../../conf/axios.js";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "../../conf/common.js";
+
+function Login(props) {
+    const [showPassword, setShowPassword] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Create the submit method.
+    const submit = async e => {
+        e.preventDefault();
+        const user = {
+            'username': username,
+            'password': password
+        }
+        const data = await axiosCall('api/token/create/', user, null, "POST")
+        localStorage.setItem(ACCESS_TOKEN_KEY, data.access)
+        localStorage.setItem(REFRESH_TOKEN_KEY, data.refresh)
+    };
+
+    function fireSetUsername(e) { setUsername(e.target.value); }
+    function fireSetPassword(e) { setPassword(e.target.value); }
+
+
+    return (
+        <div>
+            <form className="form-group" onSubmit={submit}>
+                <div>
+                    <input
+                        type="text"
+                        id="login-name-input"
+                        onChange={fireSetUsername}
+                        placeholder="Никнейм или электронная почта"
+                    />
+                </div>
+                <div>
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        id="login-pass-input"
+                        onChange={fireSetPassword}
+                        className="pass-input"
+                        placeholder="Пароль"
+                    />
+                    <img src={Eye} onClick={() => { setShowPassword(!showPassword) }} alt="" />
+                </div>
+                <p className='помошник'>
+                    <span>Забыли пароль?</span>
+                    <a className='восстановить' href='#'>Восстановить</a>
+                </p>
+                <button className='войти'>Войти</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
--- frontend/src/conf/axios.js
+++ frontend/src/conf/axios.js
@@ -1,8 +1,48 @@
 import axios from 'axios';
 import { BASE_URL } from './store.js';
+import { REFRESH_TOKEN_KEY, ACCESS_TOKEN_KEY } from './common.js';
 
-export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' }, // for POST requests
-    withCredentials: true   // for cookies:  if not set, then cookies are not sent
-});
+async function accessTokenIsValid() {
+    try {
+        const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY)
+        let response = await axiosCall(
+            "api/token/verify", 
+            { token:accessToken },
+            null, "POST"
+        )
+        console.log(response)
+        return true
+    } catch (e) {
+        console.error("Invalid acces token detected: ", e)
+        return false
+    }
+}
+
+async function refreshTokenLS() {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN_KEY)
+}
+
+
+async function axiosCall(path, data = null, headersData = null, method = "GET") {
+    try {
+        const url = BASE_URL + path
+        const headers = { 'Content-Type': 'application/json', ...headersData }
+        const response = await axios({
+            method,
+            url,
+            data,
+            headers,
+            withCredentials: true
+        });
+        return response.data;
+    } catch (error) {
+        console.error(`Error in axiosCall`);
+        console.error(error);
+    }
+}
+
+export {
+    axiosCall,
+    accessTokenIsValid,
+    refreshTokenLS
+}
