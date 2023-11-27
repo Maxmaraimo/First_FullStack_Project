29f071d650c2292199ed388741b5324993c9de0b
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2023-11-27 20:03:27 +0500

    ready footer

--- frontend/src/components/Carousel/index.jsx
+++ frontend/src/components/Carousel/index.jsx
@@ -1,37 +1,40 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import Crater from "../../assets/imgs/furniture/crater.jpg"
 import Sofa from "../../assets/imgs/furniture/sofa.jpg"
 import Table from "../../assets/imgs/furniture/table.jpg"
 import "./style.scss"
 
+
 export default function MyCarousel(props) {
+    // https://www.npmjs.com/package/react-responsive-carousel
+
 
     const width = props.width || "60%"
     const radius = props.radius || "0"
-    
+
     const imgHeight = props.imgHeight || "200px"
 
-    const style = {
-        width,
-        margin: "0 auto", 
+    const style = { 
+        width, 
+        margin: "0 auto",
         borderRadius: radius
     }
-
 
     return (
         <div id="carousel-wrapper-id" className="carousel-wrapper" style={style}>
             <div className="trends-info">
                 <p>Best furniture for your castle....</p>
 
-                <h1>New furniture Collection Trends in 2023</h1>
+                <h1>New Furniture Collection Trends in 2023</h1>
 
-                <small className="text-muted">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, corrupti deleniti iure voluptatibus suscipit non.
+                <small className="muted">
+                    {/* lorem 15 */}
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis consequatur dicta ullam fugit voluptatem nulla?
                 </small>
-                
-                <button id="but-now-btn" className="danger-btn"
-                style={{'width': '150px', 'height': '50px'}}
+
+                <button id="buy-now-btn" className="danger-btn" 
+                    style={{'width': '150px', 'height':'50px'}}
                 >
                     Shop Now
                 </button>
