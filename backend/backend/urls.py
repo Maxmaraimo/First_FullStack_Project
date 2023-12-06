35b5bc8a71ac05c28347bd717e024fe86d7f2b4e
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2023-12-06 19:29:27 +0500

    Furniture modal & FurnitureList

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -29,18 +29,18 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('furniture/', include('furniture.urls')),
+    path('api/furniture/', include('furniture.urls')),
 
 
     path('api/users/', include('users.urls')),
     path('api/token/create/', TokenObtainPairView.as_view(),
-                                                name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), 
-                                                name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), 
-                                                name='token_verify'),
-    path('api/token/blacklist/', TokenBlacklistView.as_view(), 
-                                                name='token_blacklist'),
+         name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(),
+         name='token_blacklist'),
 ]
 
 urlpatterns += static(
--- backend/furniture/models.py
+++ backend/furniture/models.py
@@ -1,13 +1,31 @@
 from django.db import models
+from PIL import Image
+import os
 
 class Furniture(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
     color = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='media/furniture/', 
-                              default='media/furniture/default.jpg')
+    image = models.ImageField(upload_to='furniture/', 
+                              default='furniture/default.jpg')
     category = models.CharField(max_length=100)
     quantity = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    def save(self, *args, **kwargs):
+        super(Furniture, self).save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        if img.height > 600 or img.width > 600:
+            output_size = (600, 600)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+    def delete(self, *args, **kwargs):
+        image_url = self.image.url
+        if image_url != '/media/furniture/default.jpg':
+            os.remove(self.image.path)
+        super().delete(*args, **kwargs)
